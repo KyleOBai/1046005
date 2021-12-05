f0ac065f74ca87e5147737e96b7a4f9eda5f93d7
Glitch (truth-stump-lamp) <none>
2021-12-05 12:41:12 +0000

    🦀🌂 Updated with Glitch

--- 1046005/search.js
+++ 1046005/search.js
@@ -1,19 +1,26 @@
 function searching() {
   var srch = document.getElementById('searchbar').value;
-  alert("Searching for article on " + srch);
   if (srch == "Terraria"){
+    alert("Searching for article on " + srch);
     window.open("subdirectories/articles/001Terraria/article.html");
   }
   else if (srch == "terraria"){
+    alert("Searching for article on " + srch);
     window.open("subdirectories/articles/001Terraria/article.html");
   }
   else if (srch == "Undertale"){
+    alert("Searching for article on " + srch);
     window.open("subdirectories/articles/002Undertale/article.html");
   }
   else if (srch == "undertale"){
+    alert("Searching for article on " + srch);
     window.open("subdirectories/articles/002Undertale/article.html");
   }
+  else if (srch == ""){
+    alert("You can't search for nothing! Please type something.");
+  }
   else{
-    alert("Invalid search. The given option may not exist. Sorry");
+    alert("Searching for article on " + srch);
+    alert("Invalid search. The given option may not exist. Or you may have made a spelling mistake.");
   }
 }
